b52f5ae6d22502fda96142e8cd610f2fea4e5aa8
IronEva2312 <agusmaty23@gmail.com>
2025-02-25 11:26:22 -0500

    Configuracion Inicial

---
+++ src/server.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+//import auth from './routers/auth_routes.js'
+
+
+
+
+// Inicializaciones
+
+const app = express();
+dotenv.config();
+
+// Configuraciones
+
+app.set('port', process.env.PORT || 4000);
+app.use(cors());
+
+// Middlewares
+app.use(express.json());
+
+
+// Variables Globales
+
+// Rutas
+
+app.get('/',(req,res)=>{
+    res.send('Server on 👨‍💻✅');
+})
+
+
+// app.use('/api/',auth)
+
+// app.use('/api/',car)
+
+// app.use('/api/',client)
+
+// app.use('/api/',booking)
+
+
+//Rutas no encontradas
+
+app.use((req,res)=>res.status(404).send("EndPoint no encontrado - 404 ❌"))
+
+// Exportar la instancia de express por medio de app
+export default  app;
